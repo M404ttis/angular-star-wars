a7c742dbe2e07add40407d2a58215a684c8ab6ed
Matthias LÃuecke <mattisluecke@gmail.com>
2024-07-09 17:54:40 +0200

    adds interface definitions

--- star-wars/src/app/star-wars-starshipI.ts
+++ star-wars/src/app/star-wars-starshipI.ts
@@ -1,2 +1,43 @@
+// reduced the interface to not contain arrays with custom types itself
 export interface StarWarsStarship {
+    MGLT:string,
+    cargo_capacity:string,
+    consumables:string,
+    cost_in_credits:string,
+    created:Date,
+    crew:string,
+    edited:Date,
+    hyperdrive_rating:string,
+    length:string,
+    manufacturer:string
+    max_atmosphering_speed:string,
+    model:string,
+    name:string,
+    passengers:string,
 }
+
+
+/*
+{
+    "MGLT": "10 MGLT",
+    "cargo_capacity": "1000000000000",
+    "consumables": "3 years",
+    "cost_in_credits": "1000000000000",
+    "created": "2014-12-10T16:36:50.509000Z",
+    "crew": "342953",
+    "edited": "2014-12-10T16:36:50.509000Z",
+    "hyperdrive_rating": "4.0",
+    "length": "120000",
+    "manufacturer": "Imperial Department of Military Research, Sienar Fleet Systems",
+    "max_atmosphering_speed": "n/a",
+    "model": "DS-1 Orbital Battle Station",
+    "name": "Death Star",
+    "passengers": "843342",
+    "films": [
+        "https://swapi.dev/api/films/1/"
+    ],
+    "pilots": [],
+    "starship_class": "Deep Space Mobile Battlestation",
+    "url": "https://swapi.dev/api/starships/9/"
+}
+*/
--- star-wars/src/app/star-wars-vehicleI.ts
+++ star-wars/src/app/star-wars-vehicleI.ts
@@ -1,2 +1,42 @@
+// quick vim tip: record macro with qq, make changes on one line,
+// enter visial line mode, apply register to all selcected lines :'<,'>normal @q
 export interface StarWarsVehicle {
+    cargo_capacity:number,
+    consumables:string,
+    cost_in_credits:number,
+    created:Date,
+    crew:string, // for whatever reason, SWAPI makes this a string
+    edited:Date,
+    length:number,
+    manufacturer:string,
+    max_atmosphering_speed:string, // for whatever reason, SWAPI makes this a string
+    model:string,
+    name:string,
+    passengers:string, // for whatever reason, SWAPI makes this a string
+    vehicle_class:string,
 }
+
+/*
+ * this is an example from the SWAPI for a Vehicle,
+ * for now we take out the films and pilots for having less interfaces
+{
+    "cargo_capacity": "50000",
+    "consumables": "2 months",
+    "cost_in_credits": "150000",
+    "created": "2014-12-10T15:36:25.724000Z",
+    "crew": "46",
+    "edited": "2014-12-10T15:36:25.724000Z",
+    "length": "36.8",
+    "manufacturer": "Corellia Mining Corporation",
+    "max_atmosphering_speed": "30",
+    "model": "Digger Crawler",
+    "name": "Sand Crawler",
+    "passengers": "30",
+    "pilots": [],
+    "films": [
+        "https://swapi.dev/api/films/1/"
+    ],
+    "url": "https://swapi.dev/api/vehicles/4/",
+    "vehicle_class": "wheeled"
+}
+*/
